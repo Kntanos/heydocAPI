b560736b053cd6e03fa5102e8c20e079c81291da
Kostas Ntanos <kostas@ntanos.co.uk>
2022-02-03 11:35:03 +0000

    Add env. variables

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ const { response } = require('express');
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
- 
+router.get('/', async (req, res) => {
+  try {
+    const response = await axios.post(procees.env.URL, process.env.TOKEN_REQ)
+    console.log(response.data.data.signIn.token);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
