9faff40929b0877fdb4ec38502d150382d9cfd8f
Kostas Ntanos <kostas@ntanos.co.uk>
2022-02-04 22:18:46 +0000

    Simple name list called from API

--- routes/index.js
+++ routes/index.js
@@ -3,52 +3,42 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 
-router.get('/', async (req, res) => {
-  try {
-    const response = await axios.post(process.env.URL,
-      {"query":`mutation {\n signIn (apiKey: "${process.env.API_KEY}", password: "${process.env.PASSWORD}")\n {\n token\n }\n}`})
-      console.log(response.data.data.signIn.token);
-  } catch (error) {
-      console.log(error);
-  }
-});
-
-// const headers = {
-//   "x-token": `${token.last}`
-//   // "x-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTM3OGU1NzMzNTdlZDAwMTU2NDhhNjIiLCJyb2xlIjoicHJhY3RpdGlvbmVyIiwiaWF0IjoxNjQzODk3MTIzLCJleHAiOjE2NDM5NDAzMjN9.Xb4kUd4QecvTBgpGFh2YkPcjMu-_C8kODzMlN9IP8c8"
-// };
-
-// const graphqlQuery = { query:
-//   `query contact($id: ID!) {
-//     contact(id: $id) {
-//       id
-//       title
-//       status
-//       firstName
-//       lastName
-//       fullName
-//       email
-//       medicalSpecialty
-//       company
-//       createdAt
-//       updatedAt
-//       }
-//     }`,
-//   variables: `{"id": 10}`
-// }
-
-// router.get('/send', async (req, res) => {
+// router.get('/', async (req, res) => {
 //   try {
-//     const response = await axios({
-//       url: process.env.URL,
-//       method: "post",
-//       headers: headers,
-//       data: graphqlQuery
-//     })
-//     console.log(response.data)
+//     const response = await axios.post(process.env.URL,
+//       {"query":`mutation {\n signIn (apiKey: "${process.env.API_KEY}", password: "${process.env.PASSWORD}")\n {\n token\n }\n}`})
+//       console.log(response.data.data.signIn.token);
 //   } catch (error) {
-//     console.log(error);
+//       console.log(error);
 //   }
 // });
+
+const headers = {
+  "x-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTM3OGU1NzMzNTdlZDAwMTU2NDhhNjIiLCJyb2xlIjoicHJhY3RpdGlvbmVyIiwiaWF0IjoxNjQ0MDA1OTkyLCJleHAiOjE2NDQwNDkxOTJ9.pGChFSWX9NhKcNbqbewuKgX7n1bfpGVBZQyE6LPQlmo"
+};
+
+const graphqlQuery = { query:
+  `query patients {
+    patients {
+      data {
+        firstName
+      }
+    }
+  }`
+}
+
+router.get('/', async (req, res) => {
+  try {
+    const response = await axios({
+      url: process.env.URL,
+      method: "post",
+      headers: headers,
+      data: graphqlQuery
+    })
+    console.log(response.data.data.patients.data)
+  } catch (error) {
+    console.log('error');
+  }
+});
 
 module.exports = router;
