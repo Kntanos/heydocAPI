24f60702464a0c800579e2583848c74afe4cd0be
Kostas Ntanos <kostas@ntanos.co.uk>
2022-02-06 20:55:24 +0000

    Extract callHeydocAPI from the route

--- routes/index.js
+++ routes/index.js
@@ -1,40 +1,13 @@
 const { response } = require('express');
 const express = require('express');
+const callHeydocAPI = require('../src/lib/callHeydocAPI');
 const router = express.Router();
-const axios = require('axios');
 const getToken = require('../src/lib/getToken');
 
 router.get('/', getToken, async (req, res) => {
 
-  const graphqlQuery = { query:
-    `query patients {
-      patients{
-        data {
-          id
-          bookings(start: "2021", end: "2023") {
-            start
-            appointment {
-              title
-            }
-          }
-        }
-      }
-    }`
-  };
-
-  try {
-    const response = await axios({
-      url: process.env.URL,
-      method: "post",
-      headers: {
-        "x-token": `${req.token}`
-      },
-      data: graphqlQuery
-    })
-    console.log(response.data.data.patients.data)
-  } catch (error) {
-    console.log(error);
-  }
+  const data = await callHeydocAPI(req, res);
+  res.send(data)
 });
 
 module.exports = router;
---
+++ src/lib/callHeydocAPI.js
@@ -0,0 +1,37 @@
+const axios = require('axios');
+
+const callHeydocAPI = async (req, res) => {
+  const graphqlQuery = { query:
+    `query patients {
+      patients{
+        data {
+          id
+          bookings(start: "2021", end: "2023") {
+            start
+            appointment {
+              title
+            }
+          }
+        }
+      }
+    }`
+  };
+
+  try {
+    const response = await axios({
+      url: process.env.URL,
+      method: "post",
+      headers: {
+        "x-token": `${req.token}`
+      },
+      data: graphqlQuery
+    })
+
+    return response.data.data.patients.data
+  
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+module.exports = callHeydocAPI;
